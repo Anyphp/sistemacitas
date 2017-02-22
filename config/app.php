f486e8fc930d44241b39e7e95dbd14d6aac2c354
Ana Muñoz <anycmt@gmail.com>
2017-02-22 18:37:29 -0400

    auth, migraciones de auth, permission, migraciones de los permisos, cambio de lang a es

--- config/app.php
+++ config/app.php
@@ -77,7 +77,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+         Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
